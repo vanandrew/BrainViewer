e2317a7baf436da4732cbd33c36f1b0c76a023ff
Andrew Van <vanandrew@wustl.edu>
2019-07-04 02:47:15 -0500

    use dark theme

--- brainviewer.py
+++ brainviewer.py
@@ -9,8 +9,9 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider,Button
 
-# eliminate toolbar when rendering window
+# set matplotlib global settings 
 mpl.rcParams['toolbar'] = 'None'
+mpl.rcParams['text.color'] = '#DDDDDD'
 
 """
     Defines an updater class so that we can multi-purpose our
@@ -56,7 +57,7 @@ class BrainViewer:
         # Create figure and axes
         self.f = plt.figure(figsize=(w,h))
         self.f.canvas.set_window_title('Brain Viewer')
-        self.f.patch.set_facecolor('#BBBBBB')
+        self.f.patch.set_facecolor('#000000')
         self.ax1 = self.f.add_axes([0.06,0.20,0.28,0.75]); self.ax1.axis('off')
         self.ax2 = self.f.add_axes([0.36,0.20,0.28,0.75]); self.ax2.axis('off')
         self.ax3 = self.f.add_axes([0.66,0.20,0.28,0.75]); self.ax3.axis('off')
@@ -69,18 +70,18 @@ class BrainViewer:
         Tinit = int(dim[2]/2)
 
         # Create sliders and buttons
-        self.slider1 = Slider(self.f.add_axes([0.1,0.15,0.2,0.03],xticks=[],yticks=[]),
-            'S',0,dim[0]-1,valinit=Sinit,valstep=1,valfmt='%1.0f')
-        self.slider2 = Slider(self.f.add_axes([0.4,0.15,0.2,0.03],xticks=[],yticks=[]),
-            'C',0,dim[1]-1,valinit=Cinit,valstep=1,valfmt='%1.0f')
-        self.slider3 = Slider(self.f.add_axes([0.7,0.15,0.2,0.03],xticks=[],yticks=[]),
-            'T',0,dim[2]-1,valinit=Tinit,valstep=1,valfmt='%1.0f')
-        self.button1down = Button(self.f.add_axes([0.1,0.1,0.1,0.05]),'-', hovercolor='0.9')
-        self.button1up = Button(self.f.add_axes([0.2,0.1,0.1,0.05]),'+', hovercolor='0.9')
-        self.button2down = Button(self.f.add_axes([0.4,0.1,0.1,0.05]),'-', hovercolor='0.9')
-        self.button2up = Button(self.f.add_axes([0.5,0.1,0.1,0.05]),'+', hovercolor='0.9')
-        self.button3down = Button(self.f.add_axes([0.7,0.1,0.1,0.05]),'-', hovercolor='0.9')
-        self.button3up = Button(self.f.add_axes([0.8,0.1,0.1,0.05]),'+', hovercolor='0.9')
+        self.slider1 = Slider(self.f.add_axes([0.1,0.15,0.2,0.03],xticks=[],yticks=[],facecolor='#222222'),
+            'S',0,dim[0]-1,valinit=Sinit,valstep=1,valfmt='%1.0f',color='#444444')
+        self.slider2 = Slider(self.f.add_axes([0.4,0.15,0.2,0.03],xticks=[],yticks=[],facecolor='#222222'),
+            'C',0,dim[1]-1,valinit=Cinit,valstep=1,valfmt='%1.0f',color='#444444')
+        self.slider3 = Slider(self.f.add_axes([0.7,0.15,0.2,0.03],xticks=[],yticks=[],facecolor='#222222'),
+            'T',0,dim[2]-1,valinit=Tinit,valstep=1,valfmt='%1.0f',color='#444444')
+        self.button1down = Button(self.f.add_axes([0.1,0.1,0.1,0.05]),'-',color='#222222',hovercolor='#333333')
+        self.button1up = Button(self.f.add_axes([0.2,0.1,0.1,0.05]),'+',color='#222222',hovercolor='#333333')
+        self.button2down = Button(self.f.add_axes([0.4,0.1,0.1,0.05]),'-',color='#222222',hovercolor='#333333')
+        self.button2up = Button(self.f.add_axes([0.5,0.1,0.1,0.05]),'+',color='#222222',hovercolor='#333333')
+        self.button3down = Button(self.f.add_axes([0.7,0.1,0.1,0.05]),'-',color='#222222',hovercolor='#333333')
+        self.button3up = Button(self.f.add_axes([0.8,0.1,0.1,0.05]),'+',color='#222222',hovercolor='#333333')
 
         # add event listeners for each control
         self.slider1.on_changed(Updater((self.ax1,lambda x: data[x,:,:].T),change_image))
